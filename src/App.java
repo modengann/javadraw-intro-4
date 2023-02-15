1ecf10b89a4181fad1eaceca7ca5a132317651c1
modengann <99417989+modengann@users.noreply.github.com>
2023-02-15 09:38:32 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,15 @@
+/**
+ * YouTube: https://youtu.be/2WxLSmKQ5T4
+ * Task: Write a method that will draw a smiley face to the screen to the center of the screen
+ *      Copy and modify text from https://noahcoetsee.gitbook.io/javadraw/guides/quickstart-examples
+ *      Decrease the size
+ */
+
+ import javadraw.*;
+
+ public class App extends Window{
+ 
+     public static void main(String[] args) {
+         
+     }
+ }
